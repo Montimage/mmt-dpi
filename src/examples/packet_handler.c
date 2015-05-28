fb0c32a3432ff512f0b6a47f191abc8be97d9b93
Luong Nguyen <luongnv89@gmail.com>
2015-05-28 10:49:34 +0200

    Added packet_handler example
    Former-commit-id: 9bce9346b4ccae558f53c5544fdfc28645370f91

---
+++ src/examples/packet_handler.c
@@ -0,0 +1,92 @@
+/**
+ * This example is intended to show a simple packet handler - show the size of received packets. That is a callback function that will be called after the processing of every packet by the MMT core
+ * 
+ * To run this example, mmt-sdk installing is required. After installing mmt-sdk, add the mmt library to project library path by following command:
+ * 
+ * $ export LD_LIBRARY_PATH=/opt/mmt/lib:/usr/local/lib:$LD_LIBRARY_PATH
+ * 
+ * Compile this example with:
+ * 
+ * $ gcc -g -I/opt/mmt/include -o packet_handler packet_handler.c -L/opt/mmt/lib -lmmt_core -ldl -lpcap
+ * 
+ * Also need to copy TCPIP plugin to plugins folder:
+ * 
+ * $ mkdir plugins
+ * 
+ * $ cp /opt/mmt/lib/libmmt_tcpip.so.0.99 plugins/libmmt_tcpip.so
+ * 
+ * And get a data file (.pcap file) by using wireShark application to capture some packet.
+ * 
+ * Then execute the program:
+ * 
+ * $ ./packet_handler tcp_plugin_image.pcap > packhdler_output.txt
+ * 
+ * The example output result in the file: packhdler_output.txt
+ * 
+ * That is it!
+ * 
+ */
+ 
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <pcap.h>
+ #include "mmt_core.h"
+
+void packet_handler(const ipacket_t * ipacket, void * user_args){
+	uint32_t * p_len = (uint32_t *) get_attribute_extracted_data_by_name(ipacket,"META","PACKET_LEN");
+	if(p_len){
+        printf("Received packet of size %u\n",*p_len);
+	}
+}
+
+int main(int argc, char ** argv){
+	mmt_handler_t *mmt_handler;// MMT handler
+	char mmt_errbuf[1024];
+	struct pkthdr header; // MMT packet header
+
+	pcap_t *pcap;
+	const unsigned char *data;
+	struct pcap_pkthdr p_pkthdr;
+	char errbuf[1024];
+
+	//Initialize MMT
+	init_extraction();
+
+	//Initialize MMT handler
+	mmt_handler =mmt_init_handler(DLT_EN10MB,0,mmt_errbuf);
+	if(!mmt_handler){
+		fprintf(stderr, "MMT handler init failed for the following reason: %s\n",mmt_errbuf );
+		return EXIT_FAILURE;
+	}
+	//Register the protocol attributes we need
+	register_extraction_attribute_by_name(mmt_handler,"META","PACKET_LEN"); //Request packet length. This is a META attribute
+
+	//Register a packet handler, it will be called for every processed packet
+	register_packet_handler(mmt_handler,1,packet_handler, NULL);
+
+	pcap = pcap_open_offline(argv[1],errbuf); // open offline trace
+	if(!pcap){ /* pcap error? */
+		fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
+		return;
+	}
+
+	while((data=pcap_next(pcap,&p_pkthdr))){
+		header.ts = p_pkthdr.ts;
+		header.caplen = p_pkthdr.caplen;
+		header.len = p_pkthdr.len;
+		if(!packet_process(mmt_handler,&header,data)){
+			fprintf(stderr, "Packet data extraction failure\n");
+		}
+	}
+
+	//Close the MMT handler
+	mmt_close_handler(mmt_handler);
+
+	//Close MMT
+	close_extraction();
+
+	pcap_close(pcap);
+
+	return EXIT_SUCCESS;
+
+}
