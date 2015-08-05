708255f9d79a7f16705ae50d1b2081934ff5c0de
Luong Nguyen <luongnv89@gmail.com>
2015-08-05 09:45:53 +0200

    Add mmt_test.h and mmt_test.c and tcp_packet_handler example
    Former-commit-id: 77ff2496423d318b47dbf2d732ee07729f7f981e
    Former-commit-id: 96fd5117dec6ce9c34b6583c006e88c5e9de2646
    Former-commit-id: 0a6b26ceef5c1c9c19abaa04b70f1c05a54bf8fe

---
+++ src/mmt_core/public_include/mmt_test.h
@@ -0,0 +1,22 @@
+/**
+* MMT_TEST provides some useful testing/debugging function
+*
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "mmt_core.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+ 
+/**
+ * Print payload field of a tcp packet
+ */
+void write_tcp_packet(const ipacket_t * ipacket);
