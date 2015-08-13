2a5a0acbd14fe15a8367034080a154a9b81e3147
NGUYEN VAN LUONG <luongnv89@gmail.com>
2015-08-13 17:08:26 +0200

    Completed tcp_extraction.c and added ip_extraction.c
    Former-commit-id: a081430d71d23bf918dad3b1b607c8c1bfb1c95c
    Former-commit-id: 5113cf6f0b76c05b9a748bcc65618e88835c7cd8
    Former-commit-id: cd35e71ffbbb67740e83d9d2b7c1bee7d0b91da8

--- data-set/tcp_extraction.c
+++ data-set/tcp_extraction.c
@@ -24,52 +24,11 @@
  #include <pcap.h>
  #include "mmt_core.h"
  #include "mmt/tcpip/mmt_tcpip.h"
+ #include "mmt_test_utils.h"
  
- /**
- * Print value
- */
-void printValue(int dataType,void* data){
-	switch(dataType){
-		// uint32_t
-		case 1:
-			if (data) {
-				printf("%u,", *((uint32_t * )data));
-			}else{
-				printf("NULL,");
-			}
-			break;
-		// uint16_t
-		case 2:
-			if (data) {
-				printf("%u,", *((uint16_t * )data));
-			}else{
-				printf("NULL,");
-			}
-			break;
-		// uint8_t
-		case 3:
-			if (data) {
-				printf("%u,", *((uint8_t * )data));
-			}else{
-				printf("NULL,");
-			}
-			break;
-		case 4:
-			if (data) {
-				struct timeval* tv = (struct timeval*) data;
-				printf("%ld,%ld,",(long)tv->tv_sec,(long)tv->tv_usec);
-			}else{
-				printf("NUL,NULL,");
-			}
-			break;
-		default:
-			printf("unknown,");
-	}
-}
 // #include "../src/mmt_tcpip/lib/mmt_tcpip_plugin_structs.h"
 void packet_handler(const ipacket_t * ipacket, void * user_args){
-	log_info("packet_handler is called for packet %lu",ipacket->packet_id);
-	uint32_t * p_len = (uint32_t *) get_attribute_extracted_data_by_name(ipacket,"META","PACKET_LEN");
+	// log_info("packet_handler is called for packet %lu",ipacket->packet_id);
 	uint16_t* tcp_src_port = (uint16_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SRC_PORT);
 	uint16_t * tcp_dest_port = (uint16_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_DEST_PORT);
 	uint32_t * tcp_seq_nb = (uint32_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SEQ_NB);
@@ -106,20 +65,28 @@ void packet_handler(const ipacket_t * ipacket, void * user_args){
 	printValue(1,tcp_ack_nb);
 	printValue(3,tcp_data_offset);
 	printValue(3,tcp_flags);
-	printValue(3,tcp_fin);
-	printValue(3,tcp_syn);
-	printValue(3,tcp_rst);
-	printValue(3,tcp_psh);
-	printValue(3,tcp_ack);
+	// tcp_flags is calculated from tcp_urg->tcp_fin
+	// Examples: tcp_flags == 2 -> 000010 (tcp_syn = 1)
+	// 			 tcp_flags == 16 -> 010000	(tcp_ack = 1)
+	// 			 tcp_flags == 24 -> 011000 (tcp_ack and tcp_psh)
+	// 			 tcp_flags == 17 -> 010001 (tcp_ack and tcp_fin)
 	printValue(3,tcp_urg);
+	printValue(3,tcp_ack);
+	printValue(3,tcp_psh);
+	printValue(3,tcp_rst);
+	printValue(3,tcp_syn);
+	printValue(3,tcp_fin);
+
 	printValue(3,tcp_ece);
 	printValue(3,tcp_cwr);
 	printValue(2,tcp_window);
 	printValue(2,tcp_checksum);
 	printValue(2,tcp_urg_ptr);
-	printValue(4,tcp_rtt);
+
+	// These attributes bellow are "POSITION_NOT_KNOWN"
 	printValue(1,tcp_syn_rcv);
 	printValue(1,tcp_conn_established);
+	printValue(4,tcp_rtt);
 	printf("\n");
 }
 
@@ -143,7 +110,6 @@ int main(int argc, char ** argv){
 		return EXIT_FAILURE;
 	}
 	//Register the protocol attributes we need
-	register_extraction_attribute_by_name(mmt_handler,"META","PACKET_LEN"); //Request packet length. This is a META attribute
 	register_extraction_attribute(mmt_handler,PROTO_TCP,TCP_SRC_PORT); //Request TCP source port
 	register_extraction_attribute(mmt_handler,PROTO_TCP,TCP_DEST_PORT); //Request TCP destination port 
 	register_extraction_attribute(mmt_handler,PROTO_TCP,TCP_SEQ_NB); //Request TCP sequence number
@@ -172,7 +138,7 @@ int main(int argc, char ** argv){
 		fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
 		return;
 	}
-	printf("packet_id,tcp_src_port,tcp_dest_port,tcp_seq_nb,tcp_ack_nb,tcp_data_offset,tcp_flags,tcp_fin,tcp_syn,tcp_rst,tcp_psh,tcp_ack,tcp_urg,tcp_ece,tcp_ece,tcp_cwr,tcp_window,tcp_checksum,tcp_urg_ptr,tcp_rtt->tv_sec,tcp_rtt->tv_usec,tcp_syn_rcv,tcp_conn_established\n");
+	printf("packet_id,tcp_src_port,tcp_dest_port,tcp_seq_nb,tcp_ack_nb,tcp_data_offset,tcp_flags,tcp_fin,tcp_syn,tcp_rst,tcp_psh,tcp_ack,tcp_urg,tcp_ece,tcp_cwr,tcp_window,tcp_checksum,tcp_urg_ptr,tcp_syn_rcv,tcp_conn_established,tcp_rtt->tv_sec,tcp_rtt->tv_usec\n");
 	while((data=pcap_next(pcap,&p_pkthdr))){
 		header.ts = p_pkthdr.ts;
 		header.caplen = p_pkthdr.caplen;
