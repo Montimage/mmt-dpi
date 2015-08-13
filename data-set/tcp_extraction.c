0d702a59433a23194329a6a923863fc13ed956a1
Luong Nguyen <luongnv89@gmail.com>
2015-08-13 11:14:51 +0200

    Update tcp_extraction: print all the tcp attributes
    Former-commit-id: 3eda0b37d1126433df7545cd84803a5afc5fca06
    Former-commit-id: dd8219fd05accce362b8b366875b1b1f0a8f35a9
    Former-commit-id: a94220c152ddfbff326dccaaad7e89adeebf26d8

--- data-set/tcp_extraction.c
+++ data-set/tcp_extraction.c
@@ -24,6 +24,48 @@
  #include <pcap.h>
  #include "mmt_core.h"
  #include "mmt/tcpip/mmt_tcpip.h"
+ 
+ /**
+ * Print value
+ */
+void printValue(int dataType,void* data){
+	switch(dataType){
+		// uint32_t
+		case 1:
+			if (data) {
+				printf("%u,", *((uint32_t * )data));
+			}else{
+				printf("NULL,");
+			}
+			break;
+		// uint16_t
+		case 2:
+			if (data) {
+				printf("%u,", *((uint16_t * )data));
+			}else{
+				printf("NULL,");
+			}
+			break;
+		// uint8_t
+		case 3:
+			if (data) {
+				printf("%u,", *((uint8_t * )data));
+			}else{
+				printf("NULL,");
+			}
+			break;
+		case 4:
+			if (data) {
+				struct timeval* tv = (struct timeval*) data;
+				printf("%ld,%ld,",(long)tv->tv_sec,(long)tv->tv_usec);
+			}else{
+				printf("NUL,NULL,");
+			}
+			break;
+		default:
+			printf("unknown,");
+	}
+}
 // #include "../src/mmt_tcpip/lib/mmt_tcpip_plugin_structs.h"
 void packet_handler(const ipacket_t * ipacket, void * user_args){
 	log_info("packet_handler is called for packet %lu",ipacket->packet_id);
@@ -32,45 +74,53 @@ void packet_handler(const ipacket_t * ipacket, void * user_args){
 	uint16_t * tcp_dest_port = (uint16_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_DEST_PORT);
 	uint32_t * tcp_seq_nb = (uint32_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SEQ_NB);
 	uint32_t * tcp_ack_nb = (uint32_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_ACK_NB);	
-	uint8_t * tcp_data_offset = (uint8_t *) get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_DATA_OFF); //Request TCP data offset
-	uint8_t * tcp_flags = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_FLAGS); //Request TCP flags
-	uint8_t * tcp_fin = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_FIN); //Request TCP fin flag
-	uint8_t * tcp_syn = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_SYN); //Request TCP syn flag
-	uint8_t * tcp_rst = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_RST); //Request TCP ACK number
-	uint8_t * tcp_psh = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_PSH); //Request TCP ACK number
-	uint8_t * tcp_ack = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_ACK); //Request TCP ACK number
-	uint8_t * tcp_urg = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_URG); //Request TCP ACK number
-	uint8_t * tcp_ece = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_ECE); //Request TCP ACK number
-	uint8_t * tcp_cwr = (uint8_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_CWR); //Request TCP ACK number
-	uint16_t * tcp_window = (uint16_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_WINDOW); //Request TCP ACK number
-	uint16_t * tcp_checksum = (uint16_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_CHECKSUM); //Request TCP ACK number
-	uint16_t * tcp_urg_ptr = (uint16_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_URG_PTR); //Request TCP ACK number
-	struct timeval * tcp_rtt = (struct timeval *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_RTT); //Request TCP ACK number
-	uint32_t * tcp_syn_rcv = (uint32_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_SYN_RCV); //Request TCP ACK number
-	uint32_t * tcp_conn_established = (uint32_t *)get_attribute_extracted_data(mmt_handler,PROTO_TCP,TCP_CONN_ESTABLISHED); //Request TCP ACK number
+	uint8_t * tcp_data_offset = (uint8_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_DATA_OFF); //Request TCP data offset
+	uint8_t * tcp_flags = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_FLAGS); //Request TCP flags
+	uint8_t * tcp_fin = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_FIN); //Request TCP fin flag
+	uint8_t * tcp_syn = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SYN); //Request TCP syn flag
+	uint8_t * tcp_rst = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_RST); //Request TCP ACK number
+	uint8_t * tcp_psh = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_PSH); //Request TCP ACK number
+	uint8_t * tcp_ack = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_ACK); //Request TCP ACK number
+	uint8_t * tcp_urg = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_URG); //Request TCP ACK number
+	uint8_t * tcp_ece = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_ECE); //Request TCP ACK number
+	uint8_t * tcp_cwr = (uint8_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_CWR); //Request TCP ACK number
+	uint16_t * tcp_window = (uint16_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_WINDOW); //Request TCP ACK number
+	uint16_t * tcp_checksum = (uint16_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_CHECKSUM); //Request TCP ACK number
+	uint16_t * tcp_urg_ptr = (uint16_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_URG_PTR); //Request TCP ACK number
+	struct timeval * tcp_rtt = (struct timeval *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_RTT); //Request TCP ACK number
+	uint32_t * tcp_syn_rcv = (uint32_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SYN_RCV); //Request TCP ACK number
+	uint32_t * tcp_conn_established = (uint32_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_CONN_ESTABLISHED); //Request TCP ACK number
 
 	//if(p_len){
         //	printf("Received packet of size %u\n",*p_len);
 	//}
-	if(tcp_src_port && tcp_dest_port && tcp_seq_nb && tcp_ack_nb){
-        	printf("%lu,%u,%u,%u,%u\n",ipacket->packet_id,*tcp_src_port,*tcp_dest_port,*tcp_seq_nb,*tcp_ack_nb);
-	}
+	// if(tcp_src_port && tcp_dest_port && tcp_seq_nb && tcp_ack_nb){
+ 	//      printf("%lu,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%ld,%ld,%u,%u\n",ipacket->packet_id,*tcp_src_port,*tcp_dest_port,*tcp_seq_nb,*tcp_ack_nb,*tcp_data_offset,*tcp_flags,*tcp_fin,*tcp_syn,*tcp_rst,*tcp_psh,*tcp_ack,*tcp_urg,*tcp_ece,*tcp_ece,*tcp_cwr,*tcp_window,*tcp_checksum,*tcp_urg_ptr,(long)tcp_rtt->tv_sec,(long)tcp_rtt->tv_usec,*tcp_syn_rcv,*tcp_conn_established);
+	// }
 	//struct mmt_tcpip_internal_packet_struct *packet = (mmt_tcpip_internal_packet_t *) ipacket->internal_packet;
 	//printf("Outoforder: %lu : %i\n",ipacket->packet_id,packet->tcp_outoforder);
-	/*
-	if(tcp_src_port){
-        	printf("TCP_SRC_PORT: %d\n",*tcp_src_port);
-	}
-	if(tcp_dest_port){
-        	printf("TCP_DEST_PORT: %i\n",*tcp_dest_port);
-	}
-	if(tcp_seq_nb){
-        	printf("TCP_SEQ_NB: %i\n",*tcp_seq_nb);
-	}
-	if(tcp_ack_nb){
-        	printf("TCP_ACK_NB: %i\n",*tcp_ack_nb);
-	}*/
-	
+	printf("%lu,", ipacket->packet_id);
+	printValue(2,tcp_src_port);
+	printValue(2,tcp_dest_port);
+	printValue(1,tcp_seq_nb);
+	printValue(1,tcp_ack_nb);
+	printValue(3,tcp_data_offset);
+	printValue(3,tcp_flags);
+	printValue(3,tcp_fin);
+	printValue(3,tcp_syn);
+	printValue(3,tcp_rst);
+	printValue(3,tcp_psh);
+	printValue(3,tcp_ack);
+	printValue(3,tcp_urg);
+	printValue(3,tcp_ece);
+	printValue(3,tcp_cwr);
+	printValue(2,tcp_window);
+	printValue(2,tcp_checksum);
+	printValue(2,tcp_urg_ptr);
+	printValue(4,tcp_rtt);
+	printValue(1,tcp_syn_rcv);
+	printValue(1,tcp_conn_established);
+	printf("\n");
 }
 
 int main(int argc, char ** argv){
@@ -122,7 +172,7 @@ int main(int argc, char ** argv){
 		fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
 		return;
 	}
-	printf("packet_id,src_port,dest_port,seq_nb,ack_nb\n");
+	printf("packet_id,tcp_src_port,tcp_dest_port,tcp_seq_nb,tcp_ack_nb,tcp_data_offset,tcp_flags,tcp_fin,tcp_syn,tcp_rst,tcp_psh,tcp_ack,tcp_urg,tcp_ece,tcp_ece,tcp_cwr,tcp_window,tcp_checksum,tcp_urg_ptr,tcp_rtt->tv_sec,tcp_rtt->tv_usec,tcp_syn_rcv,tcp_conn_established\n");
 	while((data=pcap_next(pcap,&p_pkthdr))){
 		header.ts = p_pkthdr.ts;
 		header.caplen = p_pkthdr.caplen;
