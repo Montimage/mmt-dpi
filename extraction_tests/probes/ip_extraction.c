903bfbbfdc846874692c8bc67df8b9ed22f3b745
NGUYEN VAN LUONG <luongnv89@gmail.com>
2015-08-20 15:22:05 +0200

    Add live capturing for tcp and ip extraction testing
    Former-commit-id: 3b347e950f94c8c3eea538edb9361b9cd6465565
    Former-commit-id: 2afe34aa1eb504caf2ab6a78eb7cbaafa2b3d279
    Former-commit-id: cbb1c7f43b601ee4c57936c08999ea91b144a14f

--- extraction_tests/probes/ip_extraction.c
+++ extraction_tests/probes/ip_extraction.c
@@ -22,11 +22,80 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <pcap.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <sys/time.h>
+ #include <fcntl.h>
+ #include <getopt.h>
+ #include <signal.h>
+ #include <errno.h>
+
+ #ifndef __FAVOR_BSD
+ # define __FAVOR_BSD
+ #endif
+
  #include "mmt_core.h"
  #include "mmt/tcpip/mmt_tcpip.h"
  #include "mmt_test_utils.h"
- 
-// #include "../src/mmt_tcpip/lib/mmt_tcpip_plugin_structs.h"
+
+ #define MAX_FILENAME_SIZE 256
+ #define TRACE_FILE 1
+ #define LIVE_INTERFACE 2
+ #define MTU_BIG (16 * 1024)
+
+ static int quiet;
+void usage(const char * prg_name) {
+	fprintf(stderr, "%s [<option>]\n", prg_name);
+	fprintf(stderr, "Option:\n");
+	fprintf(stderr, "\t-t <trace file>: Gives the trace file to analyse.\n");
+	fprintf(stderr, "\t-i <interface> : Gives the interface name for live traffic analysis.\n");
+	fprintf(stderr, "\t-q             : Be quiet (no output whatsoever, helps profiling).\n");
+	fprintf(stderr, "\t-h             : Prints this help.\n");
+	exit(1);
+}
+
+void parseOptions(int argc, char ** argv, char * filename, int * type) {
+	int opt, optcount = 0;
+	while ((opt = getopt(argc, argv, "t:i:qh")) != EOF) {
+		switch (opt) {
+			case 't':
+			optcount++;
+			if (optcount > 1) {
+				usage(argv[0]);
+			}
+			strncpy((char *) filename, optarg, MAX_FILENAME_SIZE);
+			*type = TRACE_FILE;
+			break;
+			case 'i':
+			optcount++;
+			if (optcount > 1) {
+				usage(argv[0]);
+			}
+			strncpy((char *) filename, optarg, MAX_FILENAME_SIZE);
+			*type = LIVE_INTERFACE;
+			break;
+			case 'q':
+			quiet = 1;
+			break;
+			case 'h':
+			default: usage(argv[0]);
+		}
+	}
+
+	if (filename == NULL || strcmp(filename, "") == 0) {
+		if (*type == TRACE_FILE) {
+			fprintf(stderr, "Missing trace file name\n");
+		}
+		if (*type == LIVE_INTERFACE) {
+			fprintf(stderr, "Missing network interface name\n");
+		}
+		usage(argv[0]);
+	}
+	return;
+}
+
 void packet_handler(const ipacket_t * ipacket, void * user_args){
 
 	uint8_t* ip_version = (uint8_t *) get_attribute_extracted_data(ipacket,PROTO_IP,IP_VERSION);
@@ -71,15 +140,33 @@ void packet_handler(const ipacket_t * ipacket, void * user_args){
 	printf("\n");
 }
 
+void live_capture_callback( u_char *user, const struct pcap_pkthdr *p_pkthdr, const u_char *data )
+{
+    mmt_handler_t *mmt = (mmt_handler_t*)user;
+    struct pkthdr header;
+    header.ts = p_pkthdr->ts;
+    header.caplen = p_pkthdr->caplen;
+    header.len = p_pkthdr->len;
+    if (!packet_process( mmt, &header, data )) {
+        fprintf(stderr, "Packet data extraction failure.\n");
+    }
+}
+
 int main(int argc, char ** argv){
 	mmt_handler_t *mmt_handler;// MMT handler
 	char mmt_errbuf[1024];
 	struct pkthdr header; // MMT packet header
 
+	char filename[MAX_FILENAME_SIZE + 1];
+    int type;
+	
 	pcap_t *pcap;
 	const unsigned char *data;
 	struct pcap_pkthdr p_pkthdr;
 	char errbuf[1024];
+	
+	quiet = 0;
+	parseOptions(argc, argv, filename, &type);
 
 	//Initialize MMT
 	init_extraction();
@@ -112,20 +199,28 @@ int main(int argc, char ** argv){
 	//Register a packet handler, it will be called for every processed packet
 	register_packet_handler(mmt_handler,1,packet_handler, NULL);
 
-	pcap = pcap_open_offline(argv[1],errbuf); // open offline trace
-	if(!pcap){ /* pcap error? */
-		fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
-		return;
-	}
-	printf("packet_id,ip_version,ip_header_len,ip_proto_tos,ip_tot_len,ip_identification,ip_df_flag,ip_mf_flag,ip_frag_offset,ip_proto_ttl,ip_proto_id,ip_checksum,ip_src,ip_dst,ip_client_addr,ip_server_addr,ip_client_port,ip_server_port\n");
-	
-	while((data=pcap_next(pcap,&p_pkthdr))){
-		header.ts = p_pkthdr.ts;
-		header.caplen = p_pkthdr.caplen;
-		header.len = p_pkthdr.len;
-		if(!packet_process(mmt_handler,&header,data)){
-			fprintf(stderr, "Packet data extraction failure\n");
+	if (type == TRACE_FILE) {
+        pcap = pcap_open_offline(filename, errbuf); // open offline trace
+        if (!pcap) { /* pcap error ? */
+	        fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
+	        return EXIT_FAILURE;
+   		}
+
+	    while ((data = pcap_next(pcap, &p_pkthdr))) {
+	    	header.ts = p_pkthdr.ts;
+	    	header.caplen = p_pkthdr.caplen;
+	    	header.len = p_pkthdr.len;
+	    	if (!packet_process(mmt_handler, &header, data)) {
+	    		fprintf(stderr, "Packet data extraction failure.\n");
+	    	}
+	    }
+	} else {
+		pcap = pcap_open_live(filename, MTU_BIG, 1, 1000, errbuf);
+		if (!pcap) {
+			fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
+			return EXIT_FAILURE;
 		}
+		(void)pcap_loop( pcap, -1, &live_capture_callback, (u_char*)mmt_handler );
 	}
 
 	//Close the MMT handler
--- extraction_tests/probes/tcp_extraction.c
+++ extraction_tests/probes/tcp_extraction.c
@@ -4,16 +4,16 @@
  * 
  * Compile this example with:
  * 
- * $ gcc -g -o packet_handler packet_handler.c -lmmt_core -ldl -lpcap
+ * $ gcc -g -o tcp_extraction tcp_extraction.c -lmmt_core -ldl -lpcap
  * 
  * 
  * And get a data file (.pcap file) by using wireShark application to capture some packet.
  * 
  * Then execute the program:
  * 
- * $ ./packet_handler tcp_plugin_image.pcap > packhdler_output.txt
+ * $ ./tcp_extraction tracefile.pcap > output.xls
  * 
- * The example output result in the file: packhdler_output.txt
+ * The example output result in the file: output.xls
  * 
  * That is it!
  * 
@@ -22,14 +22,32 @@
  #include <stdio.h>
  #include <stdlib.h>
  #include <pcap.h>
+ #include <string.h>
+ #include <unistd.h>
+ #include <sys/types.h>
+ #include <sys/stat.h>
+ #include <sys/time.h>
+ #include <fcntl.h>
+ #include <getopt.h>
+ #include <signal.h>
+ #include <errno.h>
+
+ #ifndef __FAVOR_BSD
+ # define __FAVOR_BSD
+ #endif
  #include "mmt_core.h"
  #include "mmt/tcpip/mmt_tcpip.h"
  #include "mmt_test_utils.h"
- 
-// #include "../src/mmt_tcpip/lib/mmt_tcpip_plugin_structs.h"
-void packet_handler(const ipacket_t * ipacket, void * user_args){
-	uint16_t * ip_identification = (uint16_t *) get_attribute_extracted_data(ipacket,PROTO_IP,IP_IDENTIFICATION); //Request TCP data offset
-	// log_info("packet_handler is called for packet %lu",ipacket->packet_id);
+
+ #define MAX_FILENAME_SIZE 256
+ #define TRACE_FILE 1
+ #define LIVE_INTERFACE 2
+ #define MTU_BIG (16 * 1024)
+
+ static int quiet;
+
+ void packet_handler(const ipacket_t * ipacket, void * user_args){
+	uint16_t * ip_identification = (uint16_t *) get_attribute_extracted_data(ipacket,PROTO_IP,IP_IDENTIFICATION); //Request IP packet identification
 	uint16_t* tcp_src_port = (uint16_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SRC_PORT);
 	uint16_t * tcp_dest_port = (uint16_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_DEST_PORT);
 	uint32_t * tcp_seq_nb = (uint32_t *) get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SEQ_NB);
@@ -51,14 +69,6 @@ void packet_handler(const ipacket_t * ipacket, void * user_args){
 	uint32_t * tcp_syn_rcv = (uint32_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_SYN_RCV); //Request TCP ACK number
 	uint32_t * tcp_conn_established = (uint32_t *)get_attribute_extracted_data(ipacket,PROTO_TCP,TCP_CONN_ESTABLISHED); //Request TCP ACK number
 
-	//if(p_len){
-        //	printf("Received packet of size %u\n",*p_len);
-	//}
-	// if(tcp_src_port && tcp_dest_port && tcp_seq_nb && tcp_ack_nb){
- 	//      printf("%lu,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%u,%ld,%ld,%u,%u\n",ipacket->packet_id,*tcp_src_port,*tcp_dest_port,*tcp_seq_nb,*tcp_ack_nb,*tcp_data_offset,*tcp_flags,*tcp_fin,*tcp_syn,*tcp_rst,*tcp_psh,*tcp_ack,*tcp_urg,*tcp_ece,*tcp_ece,*tcp_cwr,*tcp_window,*tcp_checksum,*tcp_urg_ptr,(long)tcp_rtt->tv_sec,(long)tcp_rtt->tv_usec,*tcp_syn_rcv,*tcp_conn_established);
-	// }
-	//struct mmt_tcpip_internal_packet_struct *packet = (mmt_tcpip_internal_packet_t *) ipacket->internal_packet;
-	//printf("Outoforder: %lu : %i\n",ipacket->packet_id,packet->tcp_outoforder);
 	printf("%lu,", ipacket->packet_id);
 	printValue(2,ip_identification);
 	printValue(2,tcp_src_port);
@@ -67,18 +77,12 @@ void packet_handler(const ipacket_t * ipacket, void * user_args){
 	printValue(1,tcp_ack_nb);
 	printValue(3,tcp_data_offset);
 	printValue(3,tcp_flags);
-	// tcp_flags is calculated from tcp_urg->tcp_fin
-	// Examples: tcp_flags == 2 -> 000010 (tcp_syn = 1)
-	// 			 tcp_flags == 16 -> 010000	(tcp_ack = 1)
-	// 			 tcp_flags == 24 -> 011000 (tcp_ack and tcp_psh)
-	// 			 tcp_flags == 17 -> 010001 (tcp_ack and tcp_fin)
 	printValue(3,tcp_urg);
 	printValue(3,tcp_ack);
 	printValue(3,tcp_psh);
 	printValue(3,tcp_rst);
 	printValue(3,tcp_syn);
 	printValue(3,tcp_fin);
-
 	printValue(3,tcp_ece);
 	printValue(3,tcp_cwr);
 	printValue(2,tcp_window);
@@ -92,15 +96,84 @@ void packet_handler(const ipacket_t * ipacket, void * user_args){
 	printf("\n");
 }
 
+void usage(const char * prg_name) {
+	fprintf(stderr, "%s [<option>]\n", prg_name);
+	fprintf(stderr, "Option:\n");
+	fprintf(stderr, "\t-t <trace file>: Gives the trace file to analyse.\n");
+	fprintf(stderr, "\t-i <interface> : Gives the interface name for live traffic analysis.\n");
+	fprintf(stderr, "\t-q             : Be quiet (no output whatsoever, helps profiling).\n");
+	fprintf(stderr, "\t-h             : Prints this help.\n");
+	exit(1);
+}
+
+void parseOptions(int argc, char ** argv, char * filename, int * type) {
+	int opt, optcount = 0;
+	while ((opt = getopt(argc, argv, "t:i:qh")) != EOF) {
+		switch (opt) {
+			case 't':
+			optcount++;
+			if (optcount > 1) {
+				usage(argv[0]);
+			}
+			strncpy((char *) filename, optarg, MAX_FILENAME_SIZE);
+			*type = TRACE_FILE;
+			break;
+			case 'i':
+			optcount++;
+			if (optcount > 1) {
+				usage(argv[0]);
+			}
+			strncpy((char *) filename, optarg, MAX_FILENAME_SIZE);
+			*type = LIVE_INTERFACE;
+			break;
+			case 'q':
+			quiet = 1;
+			break;
+			case 'h':
+			default: usage(argv[0]);
+		}
+	}
+
+	if (filename == NULL || strcmp(filename, "") == 0) {
+		if (*type == TRACE_FILE) {
+			fprintf(stderr, "Missing trace file name\n");
+		}
+		if (*type == LIVE_INTERFACE) {
+			fprintf(stderr, "Missing network interface name\n");
+		}
+		usage(argv[0]);
+	}
+	return;
+}
+
+void live_capture_callback( u_char *user, const struct pcap_pkthdr *p_pkthdr, const u_char *data )
+{
+    mmt_handler_t *mmt = (mmt_handler_t*)user;
+    struct pkthdr header;
+    header.ts = p_pkthdr->ts;
+    header.caplen = p_pkthdr->caplen;
+    header.len = p_pkthdr->len;
+    if (!packet_process( mmt, &header, data )) {
+        fprintf(stderr, "Packet data extraction failure.\n");
+    }
+}
+
+
 int main(int argc, char ** argv){
 	mmt_handler_t *mmt_handler;// MMT handler
 	char mmt_errbuf[1024];
 	struct pkthdr header; // MMT packet header
 
+	char filename[MAX_FILENAME_SIZE + 1];
+    int type;
+
 	pcap_t *pcap;
 	const unsigned char *data;
 	struct pcap_pkthdr p_pkthdr;
 	char errbuf[1024];
+
+	quiet = 0;
+	parseOptions(argc, argv, filename, &type);
 
 	//Initialize MMT
 	init_extraction();
@@ -139,25 +212,34 @@ int main(int argc, char ** argv){
 	//Register a packet handler, it will be called for every processed packet
 	register_packet_handler(mmt_handler,1,packet_handler, NULL);
 
-	pcap = pcap_open_offline(argv[1],errbuf); // open offline trace
-	if(!pcap){ /* pcap error? */
-		fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
-		return;
-	}
-	printf("packet_id,ip->ip_id,tcp_src_port,tcp_dest_port,tcp_seq_nb,tcp_ack_nb,tcp_data_offset,tcp_flags,tcp_fin,tcp_syn,tcp_rst,tcp_psh,tcp_ack,tcp_urg,tcp_ece,tcp_cwr,tcp_window,tcp_checksum,tcp_urg_ptr,tcp_syn_rcv,tcp_conn_established,tcp_rtt->tv_sec,tcp_rtt->tv_usec\n");
-	while((data=pcap_next(pcap,&p_pkthdr))){
-		header.ts = p_pkthdr.ts;
-		header.caplen = p_pkthdr.caplen;
-		header.len = p_pkthdr.len;
-		if(!packet_process(mmt_handler,&header,data)){
-			fprintf(stderr, "Packet data extraction failure\n");
+	if (type == TRACE_FILE) {
+        pcap = pcap_open_offline(filename, errbuf); // open offline trace
+        if (!pcap) { /* pcap error ? */
+	        fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
+	        return EXIT_FAILURE;
+   		}
+
+	    while ((data = pcap_next(pcap, &p_pkthdr))) {
+	    	header.ts = p_pkthdr.ts;
+	    	header.caplen = p_pkthdr.caplen;
+	    	header.len = p_pkthdr.len;
+	    	if (!packet_process(mmt_handler, &header, data)) {
+	    		fprintf(stderr, "Packet data extraction failure.\n");
+	    	}
+	    }
+	} else {
+		pcap = pcap_open_live(filename, MTU_BIG, 1, 1000, errbuf);
+		if (!pcap) {
+			fprintf(stderr, "pcap_open failed for the following reason: %s\n", errbuf);
+			return EXIT_FAILURE;
 		}
+		(void)pcap_loop( pcap, -1, &live_capture_callback, (u_char*)mmt_handler );
 	}
 
-	//Close the MMT handler
+		//Close the MMT handler
 	mmt_close_handler(mmt_handler);
 
-	//Close MMT
+		//Close MMT
 	close_extraction();
 
 	pcap_close(pcap);
