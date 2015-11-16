b374761830ffb849ddbc024c7a2bbe52623fdb91
NGUYEN VAN LUONG <luongnv89@gmail.com>
2015-11-16 14:59:52 +0100

    NDN: commented mmt_classify_me_ndn

--- src/mmt_tcpip/lib/mmt_common_internal_include.h
+++ src/mmt_tcpip/lib/mmt_common_internal_include.h
@@ -90,7 +90,7 @@ extern "C" {
     /* FTP entry */
     void mmt_classify_me_ftp(ipacket_t * ipacket, unsigned index);
     /* NDN entry */
-    void mmt_classify_me_ndn(ipacket_t * ipacket, unsigned index);
+    // void mmt_classify_me_ndn(ipacket_t * ipacket, unsigned index);
     /* USENET entry */
     void mmt_classify_me_usenet(ipacket_t * ipacket, unsigned index);
     /* DNS entry */
