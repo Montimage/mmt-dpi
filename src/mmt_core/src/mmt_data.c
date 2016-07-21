23cd3388f121158214156cab5c14b5b9e2e0027d
Luong NGUYEN <luongnv89@gmail.com>
2016-07-21 17:12:05 +0200

    Rewrite mmt_tolower and mmt_toupper function

--- src/mmt_core/src/mmt_data.c
+++ src/mmt_core/src/mmt_data.c
@@ -299,119 +299,15 @@ uint32_t short_time_diff(struct timeval *starttime, struct timeval *finishtime) 
 }
 
 char mmt_toupper(char in) {
-    switch (in) {
-        case 'a':
-            return 'A';
-        case 'b':
-            return 'B';
-        case 'c':
-            return 'C';
-        case 'd':
-            return 'D';
-        case 'e':
-            return 'E';
-        case 'f':
-            return 'F';
-        case 'g':
-            return 'G';
-        case 'h':
-            return 'H';
-        case 'i':
-            return 'I';
-        case 'j':
-            return 'J';
-        case 'k':
-            return 'K';
-        case 'l':
-            return 'L';
-        case 'm':
-            return 'M';
-        case 'n':
-            return 'N';
-        case 'o':
-            return 'O';
-        case 'p':
-            return 'P';
-        case 'q':
-            return 'Q';
-        case 'r':
-            return 'R';
-        case 's':
-            return 'S';
-        case 't':
-            return 'T';
-        case 'u':
-            return 'U';
-        case 'v':
-            return 'V';
-        case 'w':
-            return 'W';
-        case 'x':
-            return 'X';
-        case 'y':
-            return 'Y';
-        case 'z':
-            return 'Z';
-    }
-    return in;
+    if ((in >= 97) && (in <= 122))
+        in = in - 32; 
+    return in; 
 }
 
 char mmt_tolower(char in) {
-    switch (in) {
-        case 'A':
-            return 'a';
-        case 'B':
-            return 'b';
-        case 'C':
-            return 'c';
-        case 'D':
-            return 'd';
-        case 'E':
-            return 'e';
-        case 'F':
-            return 'f';
-        case 'G':
-            return 'g';
-        case 'H':
-            return 'h';
-        case 'I':
-            return 'i';
-        case 'J':
-            return 'j';
-        case 'K':
-            return 'k';
-        case 'L':
-            return 'l';
-        case 'M':
-            return 'm';
-        case 'N':
-            return 'n';
-        case 'O':
-            return 'o';
-        case 'P':
-            return 'p';
-        case 'Q':
-            return 'q';
-        case 'R':
-            return 'r';
-        case 'S':
-            return 's';
-        case 'T':
-            return 't';
-        case 'U':
-            return 'u';
-        case 'V':
-            return 'v';
-        case 'W':
-            return 'w';
-        case 'X':
-            return 'x';
-        case 'Y':
-            return 'y';
-        case 'Z':
-            return 'z';
-    }
-    return in;
+    if ((in >= 65) && (in <= 90))
+        in = in + 32; 
+    return in; 
 }
 
 
