998cd1d54b5d5f784257421fb7eebb2e85de8c04
Luong NGUYEN <luongnv89@gmail.com>
2016-12-14 11:23:28 +0100

    Change version number to 1.6.5.0

--- dist/ZIP/install.sh
+++ dist/ZIP/install.sh
@@ -4,7 +4,7 @@ if [[ $(id -u) -ne 0 ]]; then
     exit 1
 else
 	echo "Start installing mmt-sdk .... "
-	VERSION=1.6.4.0
+	VERSION=1.6.5.0
 	MMT_BASE=/opt/mmt
 	MMT_DPI=$MMT_BASE/dpi
 	MMT_LIB=$MMT_DPI/lib
