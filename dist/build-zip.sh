7792526ddbd729c17d5184b9de0ca3218c467d4c
Luong NGUYEN <luongnv89@gmail.com>
2017-03-27 15:56:09 +0200

    Update ChangeLog and version number to 1.6.6.3

--- dist/build-zip.sh
+++ dist/build-zip.sh
@@ -2,7 +2,7 @@
 sdk_dir=../sdk
 
 # Set name of package with given version
-build_dir=mmt_sdk1.6.6.2_`uname -s`_`uname -p`_`date +%s`
+build_dir=mmt_sdk1.6.6.3_`uname -s`_`uname -p`_`date +%s`
 
 echo "-]> Preparing temporary location ..."
 # Create a directory with the name of package
