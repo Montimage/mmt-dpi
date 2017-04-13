c534b8a776ad336907fd2fcc282c468151a49f6f
Luong NGUYEN <luongnv89@gmail.com>
2017-04-13 11:55:15 +0200

    Update version to 1.6.7.1

--- dist/build-deb.sh
+++ dist/build-deb.sh
@@ -1,9 +1,9 @@
-version=1.6.7.0
+version=1.6.7.1
 # Where the sdk/ compiled source code
 sdk_dir=../sdk
 
 # Set name of package with given version
-build_dir=mmt_sdk1.6.7.0_`uname -s`_`uname -p`_`date +%s`
+build_dir=mmt_sdk1.6.7.1_`uname -s`_`uname -p`_`date +%s`
 
 # Create a directory with the name of package
 mkdir $build_dir
