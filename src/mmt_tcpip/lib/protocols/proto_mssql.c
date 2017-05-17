1dcc2509583504b6511262b46e4e4ee502f34f26
Luong NGUYEN <luongnv89@gmail.com>
2017-05-17 14:51:00 +0200

    Fixed classification of PROTO_MSSQL

--- src/mmt_tcpip/lib/protocols/proto_mssql.c
+++ src/mmt_tcpip/lib/protocols/proto_mssql.c
@@ -9,12 +9,22 @@ static MMT_PROTOCOL_BITMASK detection_bitmask;
 static MMT_PROTOCOL_BITMASK excluded_protocol_bitmask;
 static MMT_SELECTION_BITMASK_PROTOCOL_SIZE selection_bitmask;
 
+// LN: TDS header
+struct tds_packet_header {
+  u_int8_t type;
+  u_int8_t status;
+  u_int16_t length;
+  u_int16_t channel;
+  u_int8_t number;
+  u_int8_t window;
+};
+// end of TDS header
 static void mmt_int_mssql_add_connection(ipacket_t * ipacket) {
     mmt_internal_add_connection(ipacket, PROTO_MSSQL, MMT_REAL_PROTOCOL);
 }
 
 void mmt_classify_me_mssql(ipacket_t * ipacket, unsigned index) {
-    
+
 
     struct mmt_tcpip_internal_packet_struct *packet = ipacket->internal_packet;
     struct mmt_internal_tcpip_session_struct *flow = packet->flow;
@@ -36,24 +46,39 @@ void mmt_classify_me_mssql(ipacket_t * ipacket, unsigned index) {
 }
 
 int mmt_check_mssql(ipacket_t * ipacket, unsigned index) {
+    printf("[debug] mmt_check_mssql: %lu, %d\n", ipacket->packet_id, index);
     struct mmt_tcpip_internal_packet_struct *packet = ipacket->internal_packet;
     if ((selection_bitmask & packet->mmt_selection_packet) == selection_bitmask
             && MMT_BITMASK_COMPARE(excluded_protocol_bitmask, packet->flow->excluded_protocol_bitmask) == 0
             && MMT_BITMASK_COMPARE(detection_bitmask, packet->detection_bitmask) != 0) {
 
-        
         struct mmt_internal_tcpip_session_struct *flow = packet->flow;
 
         MMT_LOG(PROTO_MSSQL, MMT_LOG_DEBUG, "search mssql.\n");
+        
+        // LN: Detect PROTO_MSSQL
+        if(packet->payload_packet_len > sizeof(struct tds_packet_header)){
+        
+            struct tds_packet_header *h = (struct tds_packet_header*) packet->payload;
 
-        if (packet->payload_packet_len > 51 && ntohs(get_u32(packet->payload, 0)) == 0x1201
-                && ntohs(get_u16(packet->payload, 2)) == packet->payload_packet_len
-                && ntohl(get_u32(packet->payload, 4)) == 0x00000100 && memcmp(&packet->payload[41], "sqlexpress", 10) == 0) {
-            MMT_LOG(PROTO_MSSQL, MMT_LOG_DEBUG, "found mssql.\n");
-            mmt_int_mssql_add_connection(ipacket);
-            return 1;
+            if ((h->type >= 1 && h->type <= 8) || (h->type >= 14 && h->type <= 18)) {
+                if (h->status == 0x00 || h->status == 0x01 || h->status == 0x02 || h->status == 0x04 || h->status == 0x08 || h->status == 0x09 || h->status == 0x10) {
+                    if (ntohs(h->length) == packet->payload_packet_len && h->window == 0x00) {
+                        MMT_LOG(PROTO_MSSQL, MMT_LOG_DEBUG, "found mssql.\n");
+                        mmt_int_mssql_add_connection(ipacket);
+                        return 1;
+                    }
+                }
+            }
+        // end of LN: Detect PROTO_MSSQL            
+        // if (packet->payload_packet_len > 51 && ntohs(get_u32(packet->payload, 0)) == 0x1201
+        //         && ntohs(get_u16(packet->payload, 2)) == packet->payload_packet_len
+        //         && ntohl(get_u32(packet->payload, 4)) == 0x00000100 && memcmp(&packet->payload[41], "sqlexpress", 10) == 0) {
+        //     MMT_LOG(PROTO_MSSQL, MMT_LOG_DEBUG, "found mssql.\n");
+        //     mmt_int_mssql_add_connection(ipacket);
+        //     return 1;
+        // }
         }
-
         MMT_LOG(PROTO_MSSQL, MMT_LOG_DEBUG, "exclude mssql.\n");
         MMT_ADD_PROTOCOL_TO_BITMASK(flow->excluded_protocol_bitmask, PROTO_MSSQL);
     }
