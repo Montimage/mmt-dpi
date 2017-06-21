2db372daa78c562afefdf40653f69ae65a336215
Luong NGUYEN <luongnv89@gmail.com>
2017-06-21 18:23:34 +0200

    Fixed missing function init_proto_zone_telechargement_struct

--- src/mmt_tcpip/lib/mmt_common_internal_include.h
+++ src/mmt_tcpip/lib/mmt_common_internal_include.h
@@ -2340,6 +2340,8 @@ static inline int mmt_memcmp( const void *x, const void *y, size_t size ){
     /////////// PLUGIN INIT FOR PROTO_VMWARE//////////////////
     int init_proto_vmware_struct();
     /////////////////////////////////////////////////
+    /////////// PLUGIN INIT FOR PROTO_zone_telechargement //////////////////
+    int init_proto_zone_telechargement_struct();
     ////////// END OF GENERATED CODE ////////////////
 
 #ifdef	__cplusplus
