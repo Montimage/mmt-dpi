06714de6365f829fb1347083225d2e8d26b51e64
Luong NGUYEN <luongnv89@gmail.com>
2017-06-23 12:57:13 +0200

    Change the file name format

--- dist/build-deb.sh
+++ dist/build-deb.sh
@@ -3,7 +3,7 @@ version=1.6.9.2
 sdk_dir=../sdk
 
 # Set name of package with given version
-build_dir=mmt_dpi1.6.9.2_`uname -s`_`uname -p`_`date +%s`
+build_dir=mmt-dpi_1.6.9.2_`uname -s`_`uname -p`_`date +%s`
 
 # Create a directory with the name of package
 mkdir $build_dir
