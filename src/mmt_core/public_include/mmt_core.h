0a4ec229a94cab980cd627978a0859169ff16d30
Luong NGUYEN <luongnv89@gmail.com>
2017-08-28 15:57:54 +0200

    Rollback the optimisation of mmt_memcmp: does not work

--- src/mmt_core/public_include/mmt_core.h
+++ src/mmt_core/public_include/mmt_core.h
@@ -8,7 +8,7 @@
 #ifndef MMT_CORE_H
 #define MMT_CORE_H
 
-#ifdef	__cplusplus
+#ifdef  __cplusplus
 extern "C" {
 #endif
 
@@ -71,14 +71,14 @@ extern "C" {
                             the packet will be skipped afterwards. */
 #define MMT_PRINT_INFO "\n\t* * * * * * * * * * * * * * * *\n\t*     M M T - L I B R A R Y   *\n\t* * * * * * * * * * * * * * * *\n\t\n\tWebsite: http://montimage.com\n\tContact: contact@montimage.com\n\n\n"
 #ifndef VERSION
-    #define VERSION "1.6.10.4"
+#define VERSION "1.6.10.4"
 #endif
 
 #ifdef GIT_VERSION
-    //GIT_VERSION is given by Makefile
-    #define MMT_VERSION VERSION " (" GIT_VERSION ")"
+//GIT_VERSION is given by Makefile
+#define MMT_VERSION VERSION " (" GIT_VERSION ")"
 #else
-    #define MMT_VERSION VERSION
+#define MMT_VERSION VERSION
 #endif
 
 /**
@@ -170,7 +170,7 @@ MMTAPI void MMTCALL mmt_close_handler(
 
 
 /**
- * Get number of active session 
+ * Get number of active session
  * @param  mmt_handler MMT Handler
  * @return             number of active session
  *                     -1 if the mmt_handler is NULL
@@ -613,7 +613,7 @@ MMTAPI void* MMTCALL get_attribute_extracted_data_at_index_by_name(
 );
 
 /**
- * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field ids. 
+ * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field ids.
  * The returned value is not NULL if the attribute existed in the last processed message.
  * @param ipacket pointer to the internal from which to extract the attribute.
  * @param proto_id the identifier of the protocol of the attribute.
@@ -627,7 +627,7 @@ MMTAPI attribute_t* MMTCALL get_extracted_attribute(
 );
 
 /**
- * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field names. 
+ * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field names.
  * The returned value is not NULL if the attribute existed in the last processed message.
  * @param ipacket pointer to the internal from which to extract the attribute.
  * @param protocol_name the name of the protocol of the attribute.
@@ -641,7 +641,7 @@ MMTAPI attribute_t* MMTCALL get_extracted_attribute_by_name(
 );
 
 /**
- * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field ids. 
+ * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field ids.
  * The returned value is not NULL if the attribute existed in the last processed message.
  * @param ipacket pointer to the internal from which to extract the attribute.
  * @param proto_id the identifier of the protocol of the attribute.
@@ -657,7 +657,7 @@ MMTAPI attribute_t* MMTCALL get_extracted_attribute_at_index(
 );
 
 /**
- * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field names. 
+ * Returns a pointer to the extracted attribute structure. The attribute is identified by its protocol and field names.
  * The returned value is not NULL if the attribute existed in the last processed message.
  * @param ipacket pointer to the internal from which to extract the attribute.
  * @param protocol_name the name of the protocol of the attribute.
@@ -689,8 +689,8 @@ MMTAPI proto_statistics_t* MMTCALL get_protocol_stats(
  * @param children_stats pointer to the structure where the children statistics will be reported.
  */
 MMTAPI void MMTCALL get_children_stats(proto_statistics_t * parent_stats,
-    proto_statistics_t * children_stats
-);
+                                       proto_statistics_t * children_stats
+                                      );
 
 /**
  * Prints the protocol path corresponding to the given protocol statistics instance in the given path.
@@ -853,137 +853,96 @@ MMTAPI void  MMTCALL mmt_free    ( void *x );
 
 static inline int mmt_memcmp( const void *x, const void *y, size_t size ){
     const char *s1 = (char*)x, *s2 = (char*)y;
+    int ret;
+    ret = s1[0] - s2[0];
+    if ( size == 1 || ret != 0 )
+        return ret;
 
-      if (s1[0] != s2[0]){ // 1
-        return 0;
-      }else{
-        if(size == 1) return 1; // 1
-      }
+    ret = s1[1] - s2[1];
+    if ( size == 2 || ret != 0 )
+        return ret;
 
+    ret = s1[2] - s2[2];
+    if ( size == 3 || ret != 0 )
+        return ret;
 
-      if (s1[1] != s2[1]){
-        return 0;
-      }else{
-        if(size == 2) return 1;
-      }
+    ret = s1[3] - s2[3];
+    if ( size == 4 || ret != 0 )
+        return ret;
 
-      if (s1[2] != s2[2]){
-        return 0;
-      }else{
-        if(size == 3) return 1;
-      }
+    ret = s1[4] - s2[4];
+    if ( size == 5 || ret != 0 )
+        return ret;
 
-      if (s1[3] != s2[3]){
-        return 0;
-      }else{
-        if(size == 4) return 1;
-      }
+    ret = s1[5] - s2[5];
+    if ( size == 6 || ret != 0 )
+        return ret;
 
-      if (s1[4] != s2[4]){
-        return 0;
-      }else{
-        if(size == 5) return 1;
-      }
+    ret = s1[6] - s2[6];
+    if ( size == 7 || ret != 0 )
+        return ret;
 
-      if (s1[5] != s2[5]){
-        return 0;
-      }else{
-        if(size == 6) return 1;
-      }
+    ret = s1[7] - s2[7];
+    if ( size == 8 || ret != 0 )
+        return ret;
 
-      if (s1[6] != s2[6]){
-        return 0;
-      }else{
-        if(size == 7) return 1;
-      }
+    ret = s1[8] - s2[8];
+    if ( size == 9 || ret != 0 )
+        return ret;
 
-      if (s1[7] != s2[7]){
-        return 0;
-      }else{
-        if(size == 8) return 1;
-      }
+    ret = s1[9] - s2[9];
+    if ( size == 10 || ret != 0 )
+        return ret;
 
-      if (s1[8] != s2[8]){
-        return 0;
-      }else{
-        if(size == 9) return 1;
-      }
+//0-20
+    ret = s1[10] - s2[10];
+    if ( size == 11 || ret != 0 )
+        return ret;
 
-      if (s1[9] != s2[9]){
-        return 0;
-      }else{
-        if(size == 10) return 1;
-      }
+    ret = s1[11] - s2[11];
+    if ( size == 12 || ret != 0 )
+        return ret;
 
-      if (s1[10] != s2[10]){
-        return 0;
-      }else{
-        if(size == 11) return 1;
-      }
+    ret = s1[12] - s2[12];
+    if ( size == 13 || ret != 0 )
+        return ret;
 
-      if (s1[11] != s2[11]){
-        return 0;
-      }else{
-        if(size == 12) return 1;
-      }
+    ret = s1[13] - s2[13];
+    if ( size == 14 || ret != 0 )
+        return ret;
 
-      if (s1[12] != s2[12]){
-        return 0;
-      }else{
-        if(size == 13) return 1;
-      }
+    ret = s1[14] - s2[14];
+    if ( size == 15 || ret != 0 )
+        return ret;
 
-      if (s1[13] != s2[13]){
-        return 0;
-      }else{
-        if(size == 14) return 1;
-      }
+    ret = s1[15] - s2[15];
+    if ( size == 16 || ret != 0 )
+        return ret;
 
-      if (s1[14] != s2[14]){
-        return 0;
-      }else{
-        if(size == 15) return 1;
-      }
+    ret = s1[16] - s2[16];
+    if ( size == 17 || ret != 0 )
+        return ret;
 
-      if (s1[15] != s2[15]){
-        return 0;
-      }else{
-        if(size == 16) return 1;
-      }
+    ret = s1[17] - s2[17];
+    if ( size == 18 || ret != 0 )
+        return ret;
 
-      if (s1[16] != s2[16]){
-        return 0;
-      }else{
-        if(size == 17) return 1;
-      }
+    ret = s1[18] - s2[18];
+    if ( size == 19 || ret != 0 )
+        return ret;
 
+    ret = s1[19] - s2[19];
+    if ( size == 20 || ret != 0 )
+        return ret;
 
-      if (s1[17] != s2[17]){
-        return 0;
-      }else{
-        if(size == 18) return 1;
-      }
-
-      if (s1[18] != s2[18]){
-        return 0;
-      }else{
-        if(size == 19) return 1;
-      }
-
-      if (s1[19] != s2[19]){
-        return 0;
-      }else{
-        if(size == 20) return 1;
-      }
-
-      return memcmp( s1 + 20, s2 + 20, size - 20 ) == 0;
+    return memcmp( s1 + 20, s2 + 20, size - 20 );
 }
 
 /**
  * Print Montimage information
  */
 void mmt_print_info();
-#ifdef	__cplusplus
+#ifdef  __cplusplus
 }
 #endif
 
