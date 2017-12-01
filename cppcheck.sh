56e7314d87d10df6828e6b8d4656ab96253b5b99
Luong NGUYEN <luongnv89@gmail.com>
2017-12-01 16:58:42 +0100

    Script to execute cppcheck and get output in html format

---
+++ cppcheck.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Reset mmt-probe
+WORKSPACE=/home/montimage/workspace
+PUBLIC_PATH=$WORKSPACE/express-server/public/sdk
+SRC=$WORKSPACE/montimage/mmt-sdk
+TOHTML=$WORKSPACE/montimage/mmt-test/pythons/cppcheck2html.py
+cd $SRC/
+cppcheck --xml src/* 2> cppcheck_output.xml
+mkdir -p cppcheck_$1
+python $TOHTML --file=cppcheck_output.xml --report-dir=cppcheck_$1 --title=MMT-SDK
+mv cppcheck_$1 $PUBLIC_PATH
+rm cppcheck_output.xml
