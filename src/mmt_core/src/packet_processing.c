eb2a810cde4b1445f7b36b1cd0090a70a9109b7a
Luong NGUYEN <luongnv89@gmail.com>
2018-06-01 17:51:21 +0200

    Fix free session infor of packet which does not have a session

--- src/mmt_core/src/packet_processing.c
+++ src/mmt_core/src/packet_processing.c
@@ -2976,14 +2976,13 @@ void clean_packet(ipacket_t *ipacket){
 }
 
 void clean_packet_with_reassembly(ipacket_t *ipacket){
-    //if ( (ipacket->data != ipacket->original_data) &&
-    //   (ipacket->mmt_handler->link_layer_stack->stack_id == DLT_EN10MB) ) {
-        // data was dynamically allocated during the reassembly process:
-        //   . free dynamically allocated ipacket->data
-        //   . reset ipacket->data to its original value
-	mmt_free(ipacket->proto_hierarchy);
-    mmt_free(ipacket->proto_headers_offset);
-    mmt_free(ipacket->proto_classif_status);
+
+    if(ipacket->session){ // Only packet which has session need to be clean those information
+        if (ipacket->proto_hierarchy) mmt_free(ipacket->proto_hierarchy);
+        if (ipacket->proto_headers_offset) mmt_free(ipacket->proto_headers_offset);
+        if (ipacket->proto_classif_status) mmt_free(ipacket->proto_classif_status);
+    }
+
     mmt_free(ipacket->internal_packet);
 	mmt_free((void *) ipacket->data);
         //ipacket->data = ipacket->original_data;
