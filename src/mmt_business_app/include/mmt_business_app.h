a1781bd442b1f52f257fff57b44bed92ad71aaa2
huunghia <nguyenhuunghia.gtvt@gmail.com>
2021-06-14 15:54:27 +0200

    changed proto name: ips_data to lps_data

--- src/mmt_business_app/include/mmt_business_app.h
+++ src/mmt_business_app/include/mmt_business_app.h
@@ -16,6 +16,6 @@
 #include "ips_data.h"
 
 #define PROTO_IPS_DATA 801
-#define PROTO_IPS_DATA_ALIAS "ips_data"
+#define PROTO_IPS_DATA_ALIAS "lps_data"
 
 #endif /* SRC_MMT_BUSINESS_APP_INCLUDE_MMT_BUSINESS_APP_H_ */
